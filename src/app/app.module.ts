ed8161a0e25e1978d3298d0c7a76000206eb9052
srini <srinip9066:@gmail.com>
2018-03-15 10:55:50 +0530

    Google map commits;

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,19 @@ import { MatFormFieldModule, MatInputModule } from '@angular/material';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { MatSortModule, MatTableModule } from "@angular/material";
 import {MatIconModule} from '@angular/material/icon';
+import { AgmCoreModule } from '@agm/core';
 
 
 import { AppComponent } from './app.component';
 import { WarehouseListComponent } from './warehouse-list/warehouse-list.component';
+import { MapComponent } from './map/map.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    WarehouseListComponent
+    WarehouseListComponent,
+    MapComponent
   ],
   imports: [
     BrowserModule,
@@ -24,7 +28,10 @@ import { WarehouseListComponent } from './warehouse-list/warehouse-list.componen
     MatIconModule,
     FormsModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyDcUhFqN6DcPywXTMcVzHgn6aAke_8tIJQ'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/map/map.component.ts
@@ -0,0 +1,18 @@
+import {Component} from '@angular/core';
+
+
+@Component({
+  selector: 'agm-root',
+  styles: [`
+    agm-map {
+      height: 300px;
+    }
+  `],
+  template: `
+  <agm-map [latitude]="lat" [longitude]="lng"></agm-map>
+  `
+})
+export class MapComponent {
+  lat: number = 51.678418;
+  lng: number = 7.809007;
+}
