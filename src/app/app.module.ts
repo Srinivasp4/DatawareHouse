1b916026b403a5c14d14e0670630ddb7d1bcead4
Haseena <shaikhussena222@gmail.com>
2018-03-20 23:12:41 +0530

    haseena

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,9 @@ import {MatExpansionModule} from '@angular/material/expansion';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatSortModule, MatTableModule } from "@angular/material";
 import {MatIconModule} from '@angular/material/icon';
-//import { AgmCoreModule } from '@agm/core';
+import { AgmCoreModule } from '@agm/core';
+
+
 
 // npm install ng2-charts --save
 // need to run : npm intall chat.js in case of errors related to p-chart tag
@@ -27,6 +29,9 @@ import { NotificationsComponent } from './notifications/notifications.component'
 import { SettingsComponent } from './settings/settings.component';
 import { HelpComponent } from './help/help.component';
 import { UserComponent } from './user/user.component';
+
+import {HttpClientModule} from '@angular/common/http';
+import {HttpModule} from '@angular/http';
 
 
 @NgModule({
@@ -60,7 +65,9 @@ import { UserComponent } from './user/user.component';
     RouterModule,
     AppRouterModule,
     MatExpansionModule,
-    ChartsModule
+    ChartsModule,
+    HttpClientModule,
+    HttpModule
 
   ],
   exports: [MatButtonModule, MatToolbarModule],
