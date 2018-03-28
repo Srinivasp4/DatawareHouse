6b016faae1487cf7575c58e9b421a114b4592f9b
BegamShaik <hussena.begamshaik@accenture.com>
2018-03-28 18:13:31 +0530

    enhancements

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -9,5 +9,25 @@ import { AgmCoreModule } from '@agm/core';
 export class MapComponent {
   lat: number = 17.3850;
   lng: number = 78.4867;
+
+  lat1: number = 13.0827;
+  lng1: number = 80.2707;
+
+  lat2: number = 12.9716;
+  lng2: number = 77.5946;
+
+  lat3: number = 17.3850;
+  lng3: number = 78.4867;
+  
+  lat4: number = 12.9165;
+  lng4: number = 79.1325;
+
+  lat5: number = 12.7904;
+  lng5: number = 78.7166;
+
+  lat6: number = 12.7409;
+  lng6: number = 77.8253;
   
 }
+
+
